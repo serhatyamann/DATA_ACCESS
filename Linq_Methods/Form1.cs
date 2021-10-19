998d84189542180e0f7df42c318d1d481329c59d
Serhat YAMAN <serhatyaman@hotmail.com.tr>
2021-10-20 01:41:40 +0300

    Update

--- Linq_Methods/Form1.cs
+++ Linq_Methods/Form1.cs
@@ -205,10 +205,5 @@ namespace Linq_Methods
                      TotalNumberofProducts = x.Sum(q => q.p.UnitsInStock)
                  }).ToList();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
